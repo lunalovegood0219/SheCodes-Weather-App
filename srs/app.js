d4deb02cd641303c5d3a1a5954e88c0f34f82edc
lunalovegood0219 <129015679+lunalovegood0219@users.noreply.github.com>
2023-06-01 19:55:50 +0330

    added js file
    first added axios in the html file then put real time data with API into the js file

--- srs/app.js
+++ srs/app.js
@@ -1,1 +1,36 @@
-alert("hi");
+function formatDate(timestamp) {
+  let data = new Date(timestamp);
+  let hour = data.getHours();
+  let minutes = data.getMinutes();
+  let daysOfWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let days = data.getDay();
+  return `${daysOfWeek[4]} ${hour}:${minutes}`;
+}
+function displayTemp(response) {
+  console.log(response.data);
+  let currentTempElement = document.querySelector("#currentTemp");
+  let currentCity = document.querySelector("#city");
+  let humidity = document.querySelector("#humidity");
+  let windSpeed = document.querySelector("#windSpeed");
+  let description = document.querySelector("#description");
+  let currentDate = document.querySelector("#currentDate");
+  currentTempElement.innerHTML = Math.round(response.data.main.temp);
+  currentCity.innerHTML = `${city}`;
+  humidity.innerHTML = `${response.data.main.humidity}%`;
+  windSpeed.innerHTML = `${Math.round(response.data.wind.speed)} km/h`;
+  description.innerHTML = response.data.weather[0].description;
+  currentDate.innerHTML = formatDate(response.data.dt * 1000);
+}
+let apiKey = "51f128ddb960a0cbed5d6f3eea37ad01";
+let city = "hamedan";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemp);
